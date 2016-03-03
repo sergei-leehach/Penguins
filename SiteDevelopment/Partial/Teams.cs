6c1c1b622ed2574f62d2e1e8ba4f11260af98356
sergei-leehach <sergei.leehach@gmail.com>
2016-03-03 16:58:25 +0200

    Was added entity model with NHL-table function counting

---
+++ SiteDevelopment/Partial/Teams.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteDevelopment.Models
+{
+    public partial class Teams
+    {
+        public IEnumerable<Streak> HomeGames
+        {
+            get { return HomeStreak(); }
+            private set { }
+        }
+        public IEnumerable<Streak> AwayGames
+        {
+            get { return AwayStreak(); }
+            private set { }
+        }
+        public IEnumerable<Streak> AllGames
+        {
+            get { return AllStreak(); }
+            private set { }
+        }
+
+        private IEnumerable<Streak> HomeStreak()
+        {
+            List<Streak> HomeGames = new List<Streak>();
+            foreach (var match in MatchesHome)
+            {
+                if (match.AwayTeamScore < match.HomeTeamScore)
+                {
+                    HomeGames.Add(Streak.Win);
+                }
+                else
+                {
+                    if (match.Result == TypeOfWin.OverTime || match.Result == TypeOfWin.Shootout)
+                    {
+                        HomeGames.Add(Streak.OT);
+                    }
+                    HomeGames.Add(Streak.Loss);
+                }
+            }
+            return HomeGames;
+        }
+
+        private IEnumerable<Streak> AwayStreak()
+        {
+            List<Streak> AwayGames = new List<Streak>();
+            foreach (var match in MatchesAway)
+            {
+                if (match.AwayTeamScore > match.HomeTeamScore)
+                {
+                    AwayGames.Add(Streak.Win);
+                }
+                else
+                {
+                    if (match.Result == TypeOfWin.OverTime || match.Result == TypeOfWin.Shootout)
+                    {
+                        AwayGames.Add(Streak.OT);
+                    }
+                    AwayGames.Add(Streak.Loss);
+                }
+            }
+            return AwayGames;
+        }
+
+        private IEnumerable<Streak> AllStreak()
+        {
+            List<Streak> AllGames = new List<Streak>();
+            var SortMatches = ((from m in MatchesAway select m).Union(from m in MatchesHome select m)).OrderBy(m => m.Date);
+
+            foreach (var match in SortMatches)
+            {
+                if (match.AwayTeam.Name == Name)
+                {
+                    if (match.AwayTeamScore > match.HomeTeamScore)
+                    {
+                        AllGames.Add(Streak.Win);
+                    }
+                    else
+                    {
+                        if (match.Result == TypeOfWin.OverTime || match.Result == TypeOfWin.Shootout)
+                        {
+                            AllGames.Add(Streak.OT);
+                        }
+                        AllGames.Add(Streak.Loss);
+                    }
+                }
+                else
+                {
+                    if (match.AwayTeamScore < match.HomeTeamScore)
+                    {
+                        AllGames.Add(Streak.Win);
+                    }
+                    else
+                    {
+                        if (match.Result == TypeOfWin.OverTime || match.Result == TypeOfWin.Shootout)
+                        {
+                            AllGames.Add(Streak.OT);
+                        }
+                        AllGames.Add(Streak.Loss);
+                    }
+                }
+            }
+            return AllGames;
+        }
+    }
+}
---
+++ SiteDevelopment/Repository/SiteRepository.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SiteDevelopment.Models;
+
+namespace SiteDevelopment.Repository
+{
+    public partial class SiteRepository : IDisposable
+    {
+        private Entities _db;
+
+        public SiteRepository()
+        {
+            _db = new Entities();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void NhlTableGeneration(Matches match)
+        {
+            _db.Matches.Add(match);
+
+            Teams winningTeam;
+            Teams losingTeam;
+
+            int winnersGoals;
+            int losersGoals;
+
+            if (match.AwayTeamScore > match.HomeTeamScore)
+            {
+                winningTeam = match.AwayTeam;
+                winnersGoals = match.AwayTeamScore;
+
+
+                losingTeam = match.HomeTeam;
+                losersGoals = match.HomeTeamScore;
+            }
+            else
+            {
+                winningTeam = match.HomeTeam;
+                winnersGoals = match.HomeTeamScore;
+
+                losingTeam = match.AwayTeam;
+                losersGoals = match.AwayTeamScore;
+            }
+
+            Standings winStat = (from x in winningTeam.Standings where x.Seasons.Season == "15/16" select x).FirstOrDefault();
+            Standings loseStat = (from x in losingTeam.Standings where x.Seasons.Season == "15/16" select x).FirstOrDefault();
+
+            winStat.GamesPlayed += 1;
+            loseStat.GamesPlayed += 1;
+
+            switch (match.Result)
+            {
+                case TypeOfWin.Fulltime:
+                    FulltimeCounter(match, winStat, loseStat, winnersGoals, losersGoals);
+                    break;
+                case TypeOfWin.OverTime:
+                    OvertimeCounter(match, winStat, loseStat, winnersGoals, losersGoals);
+                    break;
+                case TypeOfWin.Shootout:
+                    ShootoutCounter(match, winStat, loseStat, winnersGoals, losersGoals);
+                    break;
+            }
+        }
+
+        private void FulltimeCounter(Matches match, Standings winner, Standings loser, int winnerGoals, int loserGoals)
+        {
+            AwayHomeResult(match, winner, loser);
+
+            winner.Wins += 1;
+            winner.Points += 2;
+            winner.ROW += 1;
+            winner.GoalsFor += winnerGoals;
+            winner.GoalsAgainst += loserGoals;
+            winner.GoalDifferential += winnerGoals - loserGoals;
+
+            LastTenGamesCounter(winner);
+            StreakCounter(winner);
+
+
+            loser.Losses += 1;
+            loser.GoalsFor += loserGoals;
+            loser.GoalsAgainst += winnerGoals;
+            loser.GoalDifferential += loserGoals - winnerGoals;
+
+            LastTenGamesCounter(loser);
+            StreakCounter(loser);
+        }
+
+        private void OvertimeCounter(Matches match, Standings winner, Standings loser, int winnerGoals, int loserGoals)
+        {
+            AwayHomeResultOT(match, winner, loser);
+
+            winner.Wins += 1;
+            winner.Points += 2;
+            winner.ROW += 1;
+            winner.GoalsFor += winnerGoals;
+            winner.GoalsAgainst += loserGoals;
+            winner.GoalDifferential += winnerGoals - loserGoals;
+
+            LastTenGamesCounter(winner);
+            StreakCounter(winner);
+
+
+            loser.OT += 1;
+            loser.Points += 1;
+            loser.GoalsFor += loserGoals;
+            loser.GoalsAgainst += winnerGoals;
+            loser.GoalDifferential += loserGoals - winnerGoals;
+
+            LastTenGamesCounter(loser);
+            StreakCounter(loser);
+        }
+
+        private void ShootoutCounter(Matches match, Standings winner, Standings loser, int winnerGoals, int loserGoals)
+        {
+            AwayHomeResultOT(match, winner, loser);
+
+            winner.Wins += 1;
+            winner.Points += 2;
+            winner.GoalsFor += winnerGoals - 1;
+            winner.GoalsAgainst += loserGoals;
+
+            winner.ShootoutWins += 1;
+
+            LastTenGamesCounter(winner);
+            StreakCounter(winner);
+
+
+            loser.OT += 1;
+            loser.Points += 1;
+            loser.GoalsFor += loserGoals;
+            loser.GoalsAgainst += winnerGoals - 1;
+
+            loser.ShootoutLosses += 1;
+
+            LastTenGamesCounter(loser);
+            StreakCounter(loser);
+        }
+
+        private void AwayHomeResult(Matches match, Standings winner, Standings loser)
+        {
+            if (winner.Team.Name == match.AwayTeam.Name)
+            {
+                winner.AwayWins += 1;
+                loser.HomeLosses += 1;
+            }
+            else
+            {
+                winner.HomeWins += 1;
+                loser.AwayLosses += 1;
+            }
+        }
+
+        private void AwayHomeResultOT(Matches match, Standings winner, Standings loser)
+        {
+            if (winner.Team.Name == match.AwayTeam.Name)
+            {
+                winner.AwayWins += 1;
+                loser.HomeOT += 1;
+            }
+            else
+            {
+                winner.HomeWins += 1;
+                loser.AwayOT += 1;
+            }
+        }
+
+        [Obsolete]
+        private void AwayHomeWinner(Matches match, Standings winner)
+        {
+            if (winner.Team.Name == match.AwayTeam.Name)
+            {
+                winner.AwayWins += 1;
+            }
+            else
+            {
+                winner.HomeWins += 1;
+            }
+        }
+
+        [Obsolete]
+        private void AwayHomeLoser(Matches match, Standings loser)
+        {
+            if (match.Result == TypeOfWin.Fulltime)
+            {
+                if (loser.Team.Name == match.AwayTeam.Name)
+                {
+                    loser.AwayLosses += 1;
+                }
+                else
+                {
+                    loser.HomeLosses += 1;
+                }
+            }
+            else
+            {
+                if (loser.Team.Name == match.AwayTeam.Name)
+                {
+                    loser.AwayOT += 1;
+                }
+                else
+                {
+                    loser.HomeOT += 1;
+                }
+            }            
+        }
+
+        private void LastTenGamesCounter(Standings someTeam)
+        {
+            List<Streak> collection = someTeam.Team.AllGames.ToList();
+            collection.Reverse();
+            var games = collection.Take(10);
+
+            int wins = 0;
+            int losses = 0;
+            int OT = 0;
+
+            foreach (var game in games)
+            {
+                switch (game)
+                {
+                    case Streak.Win:
+                        wins++;
+                        break;
+                    case Streak.Loss:
+                        losses++;
+                        break;
+                    case Streak.OT:
+                        OT++;
+                        break;
+                }
+            }
+
+            someTeam.LastWins = wins;
+            someTeam.LastLosses = losses;
+            someTeam.LastOT = OT;
+        }
+
+        private void StreakCounter(Standings someTeam)
+        {
+            List<Streak> games = someTeam.Team.AllGames.ToList();
+            games.Reverse();
+
+            if (games.Count > 1)
+            {
+                if (games[1] != games[0])
+                {
+                    someTeam.Streak = 1;
+                }
+                else
+                {
+                    someTeam.Streak += 1;
+                }
+            }
+            else
+            {
+                someTeam.Streak += 1;
+            }
+        }
+    }
+}
