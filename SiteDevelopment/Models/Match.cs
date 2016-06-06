c1b99b95941470926164887ba48f50e962f506bd
sergei-leehach <sergei.leehach@gmail.com>
2016-06-06 19:44:09 +0300

    Commit

---
+++ SiteDevelopment/Models/Match.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SiteDevelopment.Models
+{
+    public partial class Match
+    {
+        [Key]
+        public int MatchId { get; set; }
+        public int HomeTeamId { get; set; }
+        public int AwayTeamId { get; set; }
+        public int HomeTeamScore { get; set; }
+        public int AwayTeamScore { get; set; }
+        public TypeOfResult Result { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Team AwayTeam { get; set; }
+        public virtual Team HomeTeam { get; set; }
+    }
+}
